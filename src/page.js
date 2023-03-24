67b48fb69efbc8f1e493c2134b830b2579f1009e
WavyBoyJodii <najeecasey@gmail.com>
2023-03-23 21:24:06 -0400

    add html plugin for favicon

--- src/page.js
+++ src/page.js
@@ -1,13 +1,34 @@
 import './style.css';
+import clipboard from './img/clipboard.png';
 
 export const contentDiv = document.getElementById('content');
+
 export const frame = document.createElement('div');
 export const frameHeader = document.createElement('div');
 export const frameBody = document.createElement('div');
+
 export const projectFrame = document.createElement('div');
 
-frame.id = 'case';
-frameHeader.classList.add('frameHead');
-frameBody.classList.add('frameBody');
+export const initPage = () => {
+  frame.id = 'case';
+  frameHeader.classList.add('frameHead');
+  const clipboardArt = new Image();
+  clipboardArt.src = clipboard;
+  clipboardArt.classList.add('frameHeaderArt');
 
-projectFrame.classList.add('projectFrame');
+  const frameTitle = document.createElement('p');
+  frameTitle.textContent = 'To Do';
+
+  frameHeader.append(clipboardArt, frameTitle);
+
+  frameBody.classList.add('frameBody');
+
+  const frameButton = document.createElement('button');
+  frameButton.classList.add('frameButton');
+
+  projectFrame.classList.add('projectFrame');
+
+  frame.append(frameHeader, frameBody, frameButton);
+
+  contentDiv.append(frame, projectFrame);
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
